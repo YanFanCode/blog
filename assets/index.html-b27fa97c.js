2bbbfc530d354e196e7e8b641d6ccd82f787eea9
YanFanCode <77973122+YanFanCode@users.noreply.github.com>
2024-03-01 09:34:01 +0000

    Deploying to gh-pages from @ YanFanCode/blog@4e217e00aa1386c79765671570d90cf27429238f 🚀

--- assets/index.html-b27fa97c.js
+++
@@ -1,4 +0,0 @@
-import{_ as n}from"./plugin-vue_export-helper-c27b6911.js";import{o,c as a,d as s,a as e,b as t}from"./app-4148853a.js";const r={},c=e("hr",null,null,-1),d=e("p",null,[t("title: 前端性能优化"),e("br"),t(" date: 2023-02-29")],-1),l=e("hr",null,null,-1),_=e("h1",{id:"前端性能优化",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#前端性能优化","aria-hidden":"true"},"#"),t(" 前端性能优化")],-1),i=e("p",null,"这里是关于前端性能优化的文章。",-1);function h(u,m){return o(),a("div",null,[s(`
- * @LastEditors: yanfan
- * @LastEditTime: 2024-02-29 16:17:48
-`),c,d,l,_,i])}const x=n(r,[["render",h],["__file","index.html.vue"]]);export{x as default};
---
+++ assets/pageview-d9a41ae6.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./app-4bf55f4a.js";const a=()=>e({serverURL:"https://blog-waline-6c6ofi82t-yanfancode.vercel.app/"});export{a as updatePageview};
